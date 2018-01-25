5e07979620ee34208d1e2b6f3f1b4afd8c96eb4e
chenjunchen <chenjunchen@chenjunchendeMacBook-Air.local>
2018-01-25 16:35:26 +0800

    登录注册版块

--- src/js/components/pc_header.js
+++ src/js/components/pc_header.js
@@ -1,18 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Row, Col } from 'antd';
-import { Menu, Icon } from 'antd';
+import {Row,Col,Menu,Icon,Tabs,Form,Input,Button,message,CheckBox} from 'antd';
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
+const FormItem = Form.Item;
 
-export default class PCHeader extends React.Component {
+class PCHeader extends React.Component {
   constructor(){
     super();
     this.state={
-      current:'toutiao'
+      current:'toutiao',//导航栏默认选择
+      modalVisible:false,//登陆框是否可视
+      action:'login',//点击按钮默认登录
+      hasLogined:false,//是否已经登录
+      userNikeName:'',//用户登录昵称
+      userid:0,
     };
   }
   render(){
+    let {getFieldProps} = this.props.form;//接受form的参数
+    const userShow = this.state.hasLogined
+    ? <Menu.Item key="logout" class="register">
+        <Button type="primary" htmlType="button">{this.state.userNikeName}</Button>//button的type可去官网看
+        &nbsp;&nbsp;
+        <Link target="_blank">
+          <Button type="dashed" htmlType="button">个人中心</Button>
+        </Link>
+        &nbsp;&nbsp;
+        <Button type="ghost" htmlType="button">退出</Button>
+      </Menu.Item>//登录之后的导航栏显示
+    : <Menu.Item key="register" class="register">
+        <Icon type="appstore" />注册/登录
+      </Menu.Item>;
     return(
       <header>
         <Row>
@@ -33,6 +52,7 @@ export default class PCHeader extends React.Component {
               <Menu.Item key="tiyu"><Icon type="appstore" />体育</Menu.Item>
               <Menu.Item key="keji"><Icon type="appstore" />科技</Menu.Item>
               <Menu.Item key="shishang"><Icon type="appstore" />时尚</Menu.Item>
+              {userShow}
             </Menu>
           </Col>
           <Col span={2}></Col>
@@ -41,3 +61,5 @@ export default class PCHeader extends React.Component {
     );
   };
 }
+
+export default PCHeader = Form.create({})(PCHeader);//form要求的二次封装
